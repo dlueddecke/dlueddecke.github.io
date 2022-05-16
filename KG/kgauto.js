571b30056de10bd64c1727ed0303f45a54b4c617
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2022-05-16 11:43:21 -0500

    Update kgauto.js

--- KG/kgauto.js
+++ KG/kgauto.js
@@ -1,21 +1,21 @@
 setInterval(function(){game.tick()},5)
-// setInterval(function(){
-//     var catpower=gamePage.resPool.get('manpower');
-//     if(catpower.value/catpower.maxValue>0.95) {
-//         gamePage.village.huntAll();
-//         if(gamePage.workshop.getCraft('parchment').unlocked) {
-//             gamePage.craftAll('parchment');
-//         }
-//         if(gamePage.workshop.getCraft('manuscript').unlocked) {
-//             gamePage.craft('manuscript', 1);
-//         }
-//         if(gamePage.workshop.getCraft('compedium').unlocked) {
-//             gamePage.craft('compedium', 1);
-//         }
-//         if(gamePage.workshop.getCraft('blueprint').unlocked) {
-//             gamePage.craft('blueprint', 1);
-//         }
-//         if(gamePage.workshop.getCraft('steel').unlocked) {
-//             gamePage.craft('steel', 1);
-//         }
-//     }},5*1000)
+setInterval(function(){
+    var catpower=gamePage.resPool.get('manpower');
+    if(catpower.value/catpower.maxValue>0.95) {
+        gamePage.village.huntAll();
+        if(gamePage.workshop.getCraft('parchment').unlocked) {
+            gamePage.craftAll('parchment');
+        }
+        if(gamePage.workshop.getCraft('manuscript').unlocked) {
+            gamePage.craft('manuscript', 1);
+        }
+        if(gamePage.workshop.getCraft('compedium').unlocked) {
+            gamePage.craft('compedium', 1);
+        }
+        if(gamePage.workshop.getCraft('blueprint').unlocked) {
+            gamePage.craft('blueprint', 1);
+        }
+        if(gamePage.workshop.getCraft('steel').unlocked) {
+            gamePage.craft('steel', 1);
+        }
+    }},5*1000)
