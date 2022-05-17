67d7718a62f1d55efedcf77756fdc38d2e11eeda
dlueddecke <dlueddecke@lightatorch.info>
2022-05-17 12:49:54 -0500

    Edited value of crafts in proportion to base materials

--- KG/kgauto2.js
+++ KG/kgauto2.js
@@ -7,27 +7,32 @@ setInterval(
         var coal = gamePage.resPool.get('coal');
         var iron = gamePage.resPool.get('iron');
         var titanium = gamePage.resPool.get('titanium');
-        var gold = gamePage.resPool.get('gold');
-        var oil = gamePage.resPool.get('oil');
-        var uranium = gamePage.resPool.get('uranium');
-        var unobtainium = gamePage.resPool.get('unobtainium');
-        var antimatter = gamePage.resPool.get('antimatter');
+        // var gold = gamePage.resPool.get('gold');
+        // var oil = gamePage.resPool.get('oil');
+        // var uranium = gamePage.resPool.get('uranium');
+        // var unobtainium = gamePage.resPool.get('unobtainium');
+        // var antimatter = gamePage.resPool.get('antimatter');
         var catpower = gamePage.resPool.get('manpower');
-        var science = gamePage.resPool.get('science');
+        // var science = gamePage.resPool.get('science');
         var culture = gamePage.resPool.get('culture');
-        var faith = gamePage.resPool.get('faith');
-        var starchart = gamePage.resPool.get('starchart');
+        // var faith = gamePage.resPool.get('faith');
+        // var starchart = gamePage.resPool.get('starchart');
 
         // var wood2 = gamePage.workshop.getCraft('wood');
         var beam = gamePage.workshop.getCraft('beam');
         var slab = gamePage.workshop.getCraft('slab');
         var plate = gamePage.workshop.getCraft('plate');
         var steel = gamePage.workshop.getCraft('steel');
-        var concrete = gamePage.workshop.getCraft('concrate');
+        // var concrete = gamePage.workshop.getCraft('concrate');
         var gear = gamePage.workshop.getCraft('gear');
         var alloy = gamePage.workshop.getCraft('alloy');
         var scaffold = gamePage.workshop.getCraft('scaffold');
-        var thorium = gamePage.workshop.getCraft('thorium');
+        // var thorium = gamePage.workshop.getCraft('thorium');
+
+        var parchment = gamePage.workshop.getCraft('parchment');
+        var manuscript = gamePage.workshop.getCraft('manuscript');
+        var compendium = gamePage.workshop.getCraft('compedium');
+        var blueprint = gamePage.workshop.getCraft('blueprint');
 
         var res = [
             [wood, catnip, 'wood'],
@@ -48,73 +53,38 @@ setInterval(
             }
         }
 
-        // [concrete, steel, 'concrate'],
-
-        // var res2 = [
-        //     [concrete, 'concrate'],
-        //     // [gear, 'gear'],
-        //     // [alloy, 'alloy']
-        //     [scaffold, 'scaffold']
+        var res2 = [
+            // [concrete, steel, 'concrate'],
+            [gear, steel, 'gear'],
+            [alloy, steel, 'alloy']
+            [scaffold, beam, 'scaffold']
         //     // [thorium, 'thorium']
-        // ]
-        //
-        // for (var i = 0; i < res2.length; i++) {
-        //     if (res2[i][0].unlocked) {
-        //         gamePage.craft(res2[i][1], 1);
-        //     }
-        // }
+        ]
 
-        // if (wood.unlocked) {
-        //     if (catnip.value / catnip.maxValue > 0.95) {gamePage.craft('wood', 1)}
-        // }
+        for (var i = 0; i < res2.length; i++) {
+            if (res2[i][0].unlocked && res2[i][1].value < res2[i][0].value) {
+                gamePage.craft(res2[i][2], 1);
+            }
+        }
 
-
-        // if (wood.unlocked) {
-        //     if (catnip.value / catnip.maxValue > 0.95) {gamePage.craft('wood', 1)}
-        // }
-        //
-        // if (beam.unlocked) {
-        //     if (wood.value / wood.maxValue > 0.95) {gamePage.craft('beam', 1)}
-        // }
-        //
-        // if (slab.unlocked) {
-        //     if (minerals.value / minerals.maxValue > 0.95) {gamePage.craft('slab', 1)}
-        // }
-        //
-        // if (plate.unlocked) {
-        //     if (iron.value / iron.maxValue > 0.95) {gamePage.craft('plate', 1)}
-        // }
-        //
-        // if (steel.unlocked) {
-        //     if (iron.value / iron.maxValue > 0.95) {gamePage.craft('plate', 1)}
-        // }
-
-
-        if (catpower.value/catpower.maxValue>0.95) {
+        if (catpower.value / catpower.maxValue > 0.95) {
             gamePage.village.huntAll();
-            if(gamePage.workshop.getCraft('parchment').unlocked) {
+            if(parchment.unlocked) {
                 gamePage.craftAll('parchment');
             }
-            if(gamePage.workshop.getCraft('manuscript').unlocked) {
-                gamePage.craft('manuscript', 1);
-            }
-            if(gamePage.workshop.getCraft('compedium').unlocked) {
-                gamePage.craft('compedium', 1);
-            }
-            if(gamePage.workshop.getCraft('blueprint').unlocked) {
-                gamePage.craft('blueprint', 1);
-            }
-            if(gamePage.workshop.getCraft('steel').unlocked) {
-                gamePage.craft('steel', 1);
-            }
         }
-
         if (culture.value / culture.maxValue > 0.99) {
-            if(gamePage.workshop.getCraft('manuscript').unlocked) {
+            if(manuscript.unlocked && manuscript.value < parchment.value) {
                 gamePage.craft('manuscript', 1);
             }
         }
+        if(compendium.unlocked && compendium.value < manuscript.value) {
+            gamePage.craft('compedium', 1);
+        }
+        if(blueprint.unlocked && blueprint.value < compendium.value) {
+            gamePage.craft('blueprint', 1);
+        }
 
-game.tick()
+    game.tick()
 
     },5)
