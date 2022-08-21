a7c29a494fcecb0f1b3f4256e288537a8a4bd2a3
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2022-08-20 21:10:38 -0500

    Update cleanUp.js

--- IXL/cleanUp.js
+++ IXL/cleanUp.js
@@ -1,2 +1,8 @@
-document.getElementById('lesson-print-button-container').remove();
+// Remove Header
 document.getElementsByClassName('site-nav-header')[0].remove();
+
+// Remove Print Button
+document.getElementById('lesson-print-button-container').remove();
+
+// Remove Social Media Buttons
+document.getElementsByClassName('lesson-header-content')[0].remove();
