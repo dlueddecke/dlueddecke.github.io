2b9570c92edf92c4253d049b7bf7cea016268f6d
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2022-08-21 19:04:39 -0500

    Update cleanUp.js

--- IXL/cleanUp.js
+++ IXL/cleanUp.js
@@ -26,3 +26,17 @@ for (var j = 0; j < pix.length; j++) {
 
 // Engage Print Window
 window.print();
+
+// Redo This
+document.getElementsByClassName('site-nav-header')[0].remove();
+document.getElementById('lesson-print-button-container').remove();
+document.getElementsByClassName('lesson-header-content')[0].remove();
+document.getElementsByClassName('sticky-section')[0].remove();
+var practice = document.getElementsByClassName('inline-skill-wrapper');
+for (var i = practice.length - 1; i >=0; i--) {practice[i].parentNode.remove();}
+document.getElementsByClassName('site-nav-footer')[0].remove();
+var pix = document.querySelectorAll('.diagramLabelContainer, .qTable, .qreactbridge, .table');
+for (var j = 0; j < pix.length; j++) {pix[j].style['break-inside'] = 'avoid';}
+var titles = document.querySelectorAll('h1');
+for (var k = 0; k < titles.length; k++) {titles[k].style['break-after'] = 'avoid-page';}
+window.print();
