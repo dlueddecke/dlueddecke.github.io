99b63b7a6a21fb452220ff3b570939492cf7c049
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2022-08-21 23:38:43 -0500

    Update cleanUp.js

--- IXL/cleanUp.js
+++ IXL/cleanUp.js
@@ -48,3 +48,14 @@ Array.from(document.querySelectorAll('.inline-skill-wrapper')).slice().reverse()
 document.querySelectorAll('.diagramLabelContainer,.qTable,.qreactbridge,.table').forEach(e=>e.style['break-inside']='avoid');
 document.querySelectorAll('h1').forEach(e=>e.style.cssText+='break-after:avoid-page;orphans:3');
 window.print();
+
+//Shortened Version #2 with @media
+document.querySelectorAll('.site-nav-header,#lesson-print-button-container,.lesson-header-content,.sticky-section,#footer-spacer,.site-nav-footer').forEach(e=>e.remove());
+document.getElementsByClassName('lesson-header-text')[0].style['width']='100%';
+document.querySelectorAll('h1').forEach(e=>e.style.cssText+='break-before:auto;break-after:avoid-page');
+Array.from(document.querySelectorAll('.inline-skill-wrapper')).slice().reverse().forEach(e=>e.parentNode.remove());
+document.querySelectorAll('.diagramLabelContainer,.qTable,.qreactbridge,.table').forEach(e=>e.style['break-inside']='avoid');
+document.querySelectorAll('.secContentPiece').forEach(e=>e.style.cssText+='break-before:auto;break-after:auto');
+document.styleSheets[0].insertRule('@media print {h1 {break-before: auto; break-after: avoid-page;}}');
+console.log(document.styleSheets[0]);
+window.print();
