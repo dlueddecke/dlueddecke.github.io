317d148e003d729a6a86c9332368fe609ecb73a4
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2022-08-31 22:42:18 -0500

    Create STYLE.js

---
+++ JEOP/STYLE.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.cell, #teams, .grid').forEach(e=>e.style.cssText+='background-color:black;');
+document.querySelectorAll('.grid-cell').forEach(e=>e.style.cssText+='background-color:orange;vertical-align:middle;');
+document.querySelectorAll('.cell').forEach(e=>e.style.cssText+='padding-top: 0px;justify-content:center;align-items:center;vertical-align:middle;');
+document.querySelectorAll('#modal-header').forEach(e=>e.style.cssText+='background-color:orange;color:black;');
+document.querySelectorAll('.modal-body').forEach(e=>e.style.cssText+='background-color:black;');
+document.querySelectorAll('#question-modal').forEach(e=>e.style.cssText+='background-color:black;');
