b07ac6a037b4776d07227db114b8b735df07ff94
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2023-01-31 22:56:45 -0600

    Create 07_unTranspose2Tables.js

---
+++ IXL/07_unTranspose2Tables.js
@@ -0,0 +1,31 @@
+// https://stackoverflow.com/questions/34685316/reorder-html-elements-in-dom
+
+function range(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => i)
+}
+
+var oldTableArray = document.getElementsByTagName('table');
+
+var n = oldTableArray.length;
+var c = 2;
+var r = Math.ceil(n / c);
+
+var oldOrder = range(0, n - 1);
+
+var everyFirst = (arr, nth) => arr.filter((e, i) => (i + 1) % nth === nth - 1);
+var everySecond = (arr, nth) => arr.filter((e, i) => i % nth === nth - 1);
+var firstCol = everyFirst(oldOrder, 3);
+var secondCol = everySecond(oldOrder, 3);
+
+var newOrder = [...firstCol, ...secondCol];
+
+var wrapper = oldTableArray[0].parentElement;
+var items = oldTableArray;
+var elements = document.createDocumentFragment();
+
+newOrder.forEach(function(idx) {
+	elements.appendChild(items[idx].cloneNode(true));
+});
+
+wrapper.innerHTML = null;
+wrapper.appendChild(elements);
