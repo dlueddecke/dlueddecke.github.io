b8eb5d7909d18a0c8c0b244f4bc36436170b3e11
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2023-01-31 22:51:13 -0600

    Create 06_transpose2Tables.js

---
+++ IXL/06_transpose2Tables.js
@@ -0,0 +1,40 @@
+// https://stackoverflow.com/questions/34685316/reorder-html-elements-in-dom
+
+function range(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => i)
+}
+
+var oldTableArray = document.getElementsByTagName('table');
+var c = 2;
+var n = oldTableArray.length;
+var r = Math.ceil(n / c);
+var oldOrder = range(0, n - 1);
+var firstCol = oldOrder.slice(0, r);
+// var y = n - r;
+// var b = Math.ceil(y / (c - 1));
+var secondCol = oldOrder.slice(r, n);
+// var thirdCol = oldOrder.slice(r + b, n);
+
+var newOrder = [];
+for (var i = 0; i < r; i++) {
+	if (typeof(firstCol[i]) === 'number') {
+		newOrder.push(firstCol[i]);
+	}
+	if (typeof(secondCol[i]) === 'number') {
+		newOrder.push(secondCol[i]);
+	}
+// 	if (typeof(thirdCol[i]) === 'number') {
+// 		newOrder.push(thirdCol[i]);
+// 	}
+}
+
+var wrapper = oldTableArray[0].parentElement;
+var items = oldTableArray;
+var elements = document.createDocumentFragment();
+
+newOrder.forEach(function(idx) {
+	elements.appendChild(items[idx].cloneNode(true));
+});
+
+wrapper.innerHTML = null;
+wrapper.appendChild(elements);
