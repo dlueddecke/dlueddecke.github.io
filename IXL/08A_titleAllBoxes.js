4e502f6c72d123d161bb3b583f6e3ba1ce0ecbd0
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2023-02-03 18:42:09 -0600

    Update 08A_titleAllBoxes.js

--- IXL/08A_titleAllBoxes.js
+++ IXL/08A_titleAllBoxes.js
@@ -1,71 +1,54 @@
 switch(true) {
 	case document.URL.endsWith('/pre-k'):
 		var newName = 'PK';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/kindergarten'):
 		var newName = 'K';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-1'):
 		var newName = '1';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-2'):
 		var newName = '2';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-3'):
 		var newName = '3';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-4'):
 		var newName = '4';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-5'):
 		var newName = '5';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-6'):
 		var newName = '6';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-7'):
 		var newName = '7';
-		var color = '#000000';
     break;
 	case document.URL.endsWith('/grade-8'):
 		var newName = '8';
-		var color = '#FFCF00';
     break;
 	case document.URL.endsWith('/algebra-1'):
 		var newName = 'A1';
-		var color = '#FC74FD';
     break;
 	case document.URL.endsWith('/geometry'):
 		var newName = 'G';
-		var color = '#80C0FF';
     break;
 	case document.URL.endsWith('/algebra-2'):
 		var newName = 'A2';	
-		var color = '#21FC0D';
     break;
 	case document.URL.endsWith('/precalculus'):
 		var newName = 'PC';    	
-		var color = '#C78FFF';
     break;
 	case document.URL.endsWith('/calculus'):
 		var newName = 'C';    	
-		var color = '#000000';
     break;		
   	default:
 		var newName = 'M';
-		var color = '#000000';
 }
 
 document.querySelectorAll('.crisp-splash-header, .skill-tree-header').forEach(e=>e.remove());
 Array.from(document.getElementsByTagName('td')).forEach(e => e.insertBefore(document.createElement('p'), e.children[0]));
-Array.from(document.getElementsByTagName('p')).forEach(e=>e.style.cssText+=`color:${color}`);
 Array.from(document.getElementsByTagName('p')).forEach(e=>e.innerHTML = `${newName}`);
 document.querySelectorAll('.skill-tree-category, .skill-tree-body, p').forEach(e=>e.style.cssText+='margin:0;padding:0');
