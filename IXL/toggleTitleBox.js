a118de1630ff977a8293ddea672434d9d9f1bf21
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2023-02-03 23:21:07 -0600

    Update toggleTitleBox.js

--- IXL/toggleTitleBox.js
+++ IXL/toggleTitleBox.js
@@ -2,6 +2,8 @@ if (document.getElementsByTagName('th').length === 0) {
 	wrap = (el, wrapper) => {el.parentNode.insertBefore(wrapper, el); wrapper.appendChild(el);}
 	Array.from(document.querySelectorAll('p')).slice().reverse().forEach(e=>wrap(e, document.createElement('th')));
 	document.querySelectorAll('th').forEach(e=>e.style.cssText+='border:1px solid #C0C0C0');
+} else if (document.getElementsByTagName('th')[0].style['border'] === '0px') {
+	document.querySelectorAll('th').forEach(e=>e.style.cssText+='border:1px solid #C0C0C0');
 } else {
 	document.querySelectorAll('th').forEach(e=>e.style.cssText+='border:0px');
 }
