2df3c37df8900433590cd2bff4f943957cbe60d7
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2023-02-03 22:30:12 -0600

    Update 08B_toggleTitlesBold.js

--- IXL/08B_toggleTitlesBold.js
+++ IXL/08B_toggleTitlesBold.js
@@ -1,4 +1,4 @@
-if (document.getElementsByTagName('p')[0].style['font-weight'] === '400') {
+if (document.getElementsByTagName('p')[0].style['font-weight'] === '400' || document.getElementsByTagName('p')[0].style['font-weight'] !== '700') {
 	Array.from(document.getElementsByTagName('p')).forEach(e=>e.style.cssText+='font-weight:700');
 } else {
 	Array.from(document.getElementsByTagName('p')).forEach(e=>e.style.cssText+='font-weight:400');
