0687d5ba8e82f6e70b7f859c067c0ece50fde460
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2023-02-03 23:15:36 -0600

    Create toggleTitleBox.js

---
+++ IXL/toggleTitleBox.js
@@ -0,0 +1,7 @@
+if (document.getElementsByTagName('th').length === 0) {
+	wrap = (el, wrapper) => {el.parentNode.insertBefore(wrapper, el); wrapper.appendChild(el);}
+	Array.from(document.querySelectorAll('p')).slice().reverse().forEach(e=>wrap(e, document.createElement('th')));
+	document.querySelectorAll('th').forEach(e=>e.style.cssText+='border:1px solid #C0C0C0');
+} else {
+	document.querySelectorAll('th').forEach(e=>e.style.cssText+='border:0px');
+}
