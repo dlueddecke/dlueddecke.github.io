d66bd946174b2770cc738f5d22c7944279f37596
dlueddecke <105024865+dlueddecke@users.noreply.github.com>
2023-02-03 23:11:00 -0600

    Update 08I_toggleTitlesItal.js

--- IXL/08I_toggleTitlesItal.js
+++ IXL/08I_toggleTitlesItal.js
@@ -1,4 +1,4 @@
-if (document.getElementsByTagName('p')[0].stye['font-style'] === 'normal' || document.getElementsByTagName('p')[0].stye['font-style'] !== 'italic') {
+if (document.getElementsByTagName('p')[0].style['font-style'] === 'normal' || document.getElementsByTagName('p')[0].style['font-style'] !== 'italic') {
 	Array.from(document.getElementsByTagName('p')).forEach(e=>e.style.cssText+='font-style:italic');
 } else {
 	Array.from(document.getElementsByTagName('p')).forEach(e=>e.style.cssText+='font-style:normal');
